00b7acd4348eacb4225000aa1692a7e2e597155f
Moksh <moksh13112006@gmail.com>
2025-09-10 01:09:35 +0530

    fic: removed mouse follower

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/custom/navbar';
 import Home from './pages/home';
-import MouseFollower from './components/custom/MouseFollower';
 import GradualBlurMemo from './components/custom/GradualBlur';
 import Header from './components/custom/Header';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <MouseFollower />
       
       {/* Fixed navbar overlay - appears on all sections */}
       <div className="fixed top-0 left-0 right-0 z-50">
